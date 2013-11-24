1c3e04355769154c7cb1a31ebfae4bc942c7bba5
omochi <omochi6644@gmail.com>
2013-11-25 04:01:56 +0900

    check for bugs. NEE probably does not have bugs, but original path tracer does (too dark)

--- src/scenes/CornellBoxScene.cpp
+++ src/scenes/CornellBoxScene.cpp
@@ -15,16 +15,17 @@ CornellBoxScene::CornellBoxScene()
   AddObject(new Sphere(1e5, Vector3(50, 40.8, -1e5+250),    Material(Material::REFLECTION_TYPE_LAMBERT, Color(), Color())), true, false);                  // éËëO
   AddObject(new Sphere(1e5, Vector3(50,  1e5,      81.6), Material(Material::REFLECTION_TYPE_LAMBERT, Color(), Color(0.75, 0.75, 0.75))), true, false);  // â∫
   AddObject(new Sphere(1e5, Vector3(50, -1e5+81.6, 81.6), Material(Material::REFLECTION_TYPE_LAMBERT, Color(), Color(0.75, 0.75, 0.75))), true, false);  // è„
-  AddObject(new Sphere(20,Vector3(50, 20, 50),           Material(Material::REFLECTION_TYPE_LAMBERT,    Color(), Color(0.25, 0.75, 0.25))));    // óŒãÖ
-  AddObject(new Sphere(16.5,Vector3(19, 16.5, 25),       Material(Material::REFLECTION_TYPE_SPECULAR,   Color(), Color(0.99, 0.99, 0.99))));   // ãæ
-  AddObject(new Sphere(16.5, Vector3(77, 16.5, 78), Material(Material::REFLECTION_TYPE_REFRACTION, Color(), Color(0.99, 0.99, 0.99), REFRACTIVE_INDEX_OBJECT))); // ÉKÉâÉX
-  SphereLight *sphereLight = new SphereLight(7.5, Vector3(50.0, 72.5, 81.6), Material(Material::REFLECTION_TYPE_LAMBERT, Color(16, 16, 16), Color()));
-  AddObject(sphereLight);    // è∆ñæ
 
-  //AddObject(new Sphere(16.5, Vector3(27, 16.5, 47), Material(Material::REFLECTION_TYPE_SPECULAR, Color(), Color(0.99, 0.99, 0.99))));   // ãæ
-  //AddObject(new Sphere(16.5, Vector3(73, 16.5, 78), Material(Material::REFLECTION_TYPE_REFRACTION, Color(), Color(0.99, 0.99, 0.99), REFRACTIVE_INDEX_OBJECT))); // ÉKÉâÉX
-  //SphereLight *sphereLight = new SphereLight(5, Vector3(50.0, 75, 81.6), Material(Material::REFLECTION_TYPE_LAMBERT, Color(12, 12, 12), Color()));
+  //AddObject(new Sphere(20,Vector3(50, 20, 50),           Material(Material::REFLECTION_TYPE_LAMBERT,    Color(), Color(0.25, 0.75, 0.25))));    // óŒãÖ
+  //AddObject(new Sphere(16.5,Vector3(19, 16.5, 25),       Material(Material::REFLECTION_TYPE_SPECULAR,   Color(), Color(0.99, 0.99, 0.99))));   // ãæ
+  //AddObject(new Sphere(16.5, Vector3(77, 16.5, 78), Material(Material::REFLECTION_TYPE_REFRACTION, Color(), Color(0.99, 0.99, 0.99), REFRACTIVE_INDEX_OBJECT))); // ÉKÉâÉX
+  //SphereLight *sphereLight = new SphereLight(7.5, Vector3(50.0, 72.5, 81.6), Material(Material::REFLECTION_TYPE_LAMBERT, Color(16, 16, 16), Color()));
   //AddObject(sphereLight);    // è∆ñæ
+
+  AddObject(new Sphere(16.5, Vector3(27, 16.5, 47), Material(Material::REFLECTION_TYPE_SPECULAR, Color(), Color(0.99, 0.99, 0.99))));   // ãæ
+  AddObject(new Sphere(16.5, Vector3(73, 16.5, 78), Material(Material::REFLECTION_TYPE_REFRACTION, Color(), Color(0.99, 0.99, 0.99), REFRACTIVE_INDEX_OBJECT))); // ÉKÉâÉX
+  SphereLight *sphereLight = new SphereLight(5, Vector3(50.0, 75, 81.6), Material(Material::REFLECTION_TYPE_LAMBERT, Color(12, 12, 12), Color()));
+  AddObject(sphereLight);    // è∆ñæ
 
   //ConstructQBVH();
 }
